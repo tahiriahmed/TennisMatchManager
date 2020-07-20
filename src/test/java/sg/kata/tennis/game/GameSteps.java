c9d140360873f0adb503d3d2a09be44a88d1cc9d
tahiriahmed <tahiri_ahmed@yahoo.com>
2020-07-20 13:26:39 +0200

    Design and creation of tests before implementing treatments

--- src/test/java/sg/kata/tennis/game/GameSteps.java
+++ src/test/java/sg/kata/tennis/game/GameSteps.java
@@ -2,6 +2,7 @@ package sg.kata.tennis.game;
 
 import org.junit.Assert;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -36,6 +37,30 @@ public class GameSteps {
 	@Then("^The second player won the match$")
 	public void the_second_player_won_the_match() throws Throwable {
 		Assert.assertTrue(gameScores.win.equals(game.getSecondPlayerScore()));
+	}
+	
+	@Then("^DEUCE rule is activated$")
+	public void deuce_rule_is_activated() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^The first player take the ADVANTAGE$")
+	public void the_first_player_take_the_ADVANTAGE() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The first player lose a point$")
+	public void the_first_player_lose_a_point() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^The score is DEUCE$")
+	public void the_score_is_DEUCE() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
 	}
 
 }
---
+++ src/test/java/sg/kata/tennis/game/UserStory2_abbreviated.feature
@@ -0,0 +1,27 @@
+Feature: Manage a tennis GAME within a set of a tennis match (abbreviated senario)
+
+  Scenario: If the 2 players reach the score 40, the DEUCE rule is activated
+    Given Game started
+    When the first player won a point
+    And the first player won a point
+    And the first player won a point
+    And the second player won a point
+    And the second player won a point
+    And the second player won a point
+    Then DEUCE rule is activated
+
+  Scenario: If the score is DEUCE , the player who win the point take the ADVANTAGE
+    Given The score is DEUCE
+    When the first player won a point
+    Then The first player take the ADVANTAGE
+
+  Scenario: If the player who has the ADVANTAGE win the point, he win the game
+    Given The score is DEUCE
+    When the first player won a point
+    And the first player won a point
+    Then The first player won the match
+
+  Scenario: If the score is DEUCE , the player who win the point take the ADVANTAGE
+    Given The score is DEUCE
+    When The first player lose a point
+    Then DEUCE rule is activated
